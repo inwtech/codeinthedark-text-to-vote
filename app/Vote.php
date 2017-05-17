195b6d0a30923a103f911fabccb0941eb7384f8c
Michael Williams <mtotheikle@gmail.com>
2017-05-16 19:55:18 -0700

    fix rounds

--- app/Vote.php
+++ app/Vote.php
@@ -12,6 +12,10 @@ class Vote extends Model
         $phone_number = preg_replace('/\D/', '', $phone_number);
         $active_round = \App\Round::where('is_current', 1)->first();
 
+        if (empty($active_round)) {
+            return 'Sorry, no active rounds yet';
+        }
+
         $has_voted = Vote::where('phone_number', $phone_number)->where('round_id', $active_round->id)->first();
         if (null != $has_voted) {
             return 'Sorry, you can only vote once.';
